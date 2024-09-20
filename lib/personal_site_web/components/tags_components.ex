56f226d402d43c86130b725817c7c04be4127b81
Jesse Claven <jesse.claven@me.com>
2024-09-20 21:06:59 +0100

    refactor(notes): Link tags consistently

---
+++ lib/personal_site_web/components/tags_components.ex
@@ -0,0 +1,17 @@
+defmodule PersonalSiteWeb.TagsComponents do
+  @moduledoc false
+
+  use Phoenix.Component
+
+  def inline(assigns) do
+    ~H"""
+    <%= for {tag, index} <- Enum.with_index(@tags) do %>
+      <%= if index == length(@tags) - 1 do %>
+        <.link navigate={"/notes/tags/#{tag}"}><%= tag %></.link>
+      <% else %>
+        <.link navigate={"/notes/tags/#{tag}"}><%= tag %></.link><span>, </span>
+      <% end %>
+    <% end %>
+    """
+  end
+end
