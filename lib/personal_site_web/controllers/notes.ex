a9fd2529146a32134daed6608fbe95ff18d29324
Jesse Claven <jesse.claven@me.com>
2025-04-30 15:42:50 +0100

    refactor(blog): Merge redirects

--- lib/personal_site_web/controllers/notes.ex
+++ lib/personal_site_web/controllers/notes.ex
@@ -8,19 +8,5 @@ defmodule PersonalSiteWeb.NotesController do
     Phoenix.Controller.redirect(conn, to: ~p"/blog")
   end
 
-  def single(conn, params) do
-    slug =
-      case params["id"] do
-        "initial-release-of-semantic-search-for-notes-app" ->
-          "initial-release-of-hybrid-search-for-notes-app"
-
-        "v1-of-raycast-extension-for-himalaya" ->
-          "v1-of-raycast-extension-for-himalaya-released"
-
-        slug ->
-          slug
-      end
-
-    Phoenix.Controller.redirect(conn, to: ~p"/blog/#{slug}")
-  end
+  def single(conn, params), do: Phoenix.Controller.redirect(conn, to: ~p"/blog/#{params["id"]}")
 end
