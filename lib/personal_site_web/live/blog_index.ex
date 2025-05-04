d48a518ef2a13e67bcc60fbfcffd3572d1c15432
Jesse Claven <jesse.claven@me.com>
2025-05-04 14:54:55 +0100

    feat(blog): Add month headers for index

--- lib/personal_site_web/live/blog_index.ex
+++ lib/personal_site_web/live/blog_index.ex
@@ -14,6 +14,12 @@ defmodule PersonalSiteWeb.Live.BlogIndex do
       all_posts
       |> Enum.group_by(& &1.date_created.year)
       |> Enum.sort_by(&elem(&1, 0), :desc)
+      |> Enum.map(fn {year, posts} ->
+        {year,
+         posts
+         |> Enum.group_by(& &1.date_created.month)
+         |> Enum.sort_by(&elem(&1, 0), :desc)}
+      end)
 
     updated =
       socket
@@ -23,28 +29,34 @@ defmodule PersonalSiteWeb.Live.BlogIndex do
     {:ok, updated}
   end
 
+  # Use an arbitrary year and day
+  defp month_name(month_num), do: Calendar.strftime(Date.new!(2021, month_num, 1), "%B")
+
   def render(assigns) do
     ~H"""
     <.live_component module={PersonalSiteWeb.Live.Cursors} id="cursors" users={@users} />
     <h1 class="text-lg"><.link navigate={~p"/blog"}>Blog</.link></h1>
     <div class="space-y-3 mt-3">
       <p class="text-sm"><.link navigate={~p"/blog/tags"}>All tags</.link></p>
-      <div :for={{year, posts} <- @years} class="space-y-1">
-        <div>{year}</div>
+      <div :for={{year, months} <- @years} class="space-y-1">
+        <div><strong>{year}</strong></div>
         <div class="space-y-3">
-          <div :for={post <- posts} class="space-y-1">
-            <p class="text-sm">
-              <.link navigate={~p"/blog/#{post.slug}"}>
-                {post.title}
-              </.link>
-            </p>
-            <p class="text-xs">
-              {post.date_created}
-              <%= if post.date_updated do %>
-                (updated: {post.date_updated})
-              <% end %>
-              ･ <PersonalSiteWeb.TagsComponents.inline tags={post.tags} />
-            </p>
+          <div :for={{month, posts} <- months} class="space-y-1">
+            <div>{month_name(month)}</div>
+            <div :for={post <- posts} class="space-y-1">
+              <p class="text-sm">
+                <.link navigate={~p"/blog/#{post.slug}"}>
+                  {post.title}
+                </.link>
+              </p>
+              <p class="text-xs">
+                {post.date_created}
+                <%= if post.date_updated do %>
+                  (updated: {post.date_updated})
+                <% end %>
+                ･ <PersonalSiteWeb.TagsComponents.inline tags={post.tags} />
+              </p>
+            </div>
           </div>
         </div>
       </div>
