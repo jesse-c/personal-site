a9d1c460ae4f088657b2939bb97c3a7fd3e01a98
Jesse Claven <jesse.claven@me.com>
2025-06-30 22:26:42 +0100

    fix: Handle 406 responses

--- lib/personal_site_web/controllers/error_html.ex
+++ lib/personal_site_web/controllers/error_html.ex
@@ -34,6 +34,19 @@ defmodule PersonalSiteWeb.ErrorHTML do
     """
   end
 
+  def render("406.html", assigns) do
+    ~H"""
+    <.root
+      inner_content={not_acceptable(assigns)}
+      flash={%{}}
+      contributions={Contributions.all_contributions()}
+      posts={Blog.all_posts()}
+      projects={Projects.all_projects()}
+      works={Works.all_works()}
+    />
+    """
+  end
+
   defp not_found(assigns) do
     ~H"""
     <p class="text-sm">Sorry, the page you are looking for does not exist.</p>
@@ -43,6 +56,14 @@ defmodule PersonalSiteWeb.ErrorHTML do
   defp internal_server_error(assigns) do
     ~H"""
     <p class="text-sm">Sorry, we've encountered an internal error.</p>
+    """
+  end
+
+  defp not_acceptable(assigns) do
+    ~H"""
+    <p class="text-sm">
+      Sorry, the requested format is not acceptable. The server cannot produce a response matching the list of acceptable values.
+    </p>
     """
   end
 end
