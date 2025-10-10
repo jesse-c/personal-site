289b5d01c338b7946b704522167b0b5d120d9a4a
Jesse Claven <jesse.claven@me.com>
2025-10-10 16:09:06 +0100

    feat: Build Sentry map

--- config/prod.exs
+++ config/prod.exs
@@ -13,5 +13,9 @@ config :logger, level: :info
 
 config :personal_site, enable_sentry: true
 
+config :sentry,
+  enable_source_code_context: true,
+  root_source_code_paths: [File.cwd!()]
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
